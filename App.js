aea12ab79bd22a98367c90dc5d1e3ec15b6ab9f1
md-faid <md.farid.ice@gmail.com>
2021-09-07 00:56:27 +0600

    add simple button here

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-/** kano aslo na */
-export default function App() {
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  const [outputText, setOutputText] = useState(
+    'Open up App.js to start working on your app!'
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{outputText}</Text>
+      <Button
+        title="Change Text"
+        onPress={() => setOutputText('The text changed!')}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
+
+export default App;
